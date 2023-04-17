877f7283205b42a39b945cc9aa383ec14a29df1c
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2023-04-17 13:28:11 +0530

    first n last commit

---
+++ src/main/java/com/skillwise/skillwisegateway/SkillwisegatewayApplication.java
@@ -0,0 +1,13 @@
+package com.skillwise.skillwisegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillwisegatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillwisegatewayApplication.class, args);
+    }
+
+}
